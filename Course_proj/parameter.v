8ab339f2e7e8cd75035da516962cd5e0e850f762
Rohit-kademani <83585335+Rohit-kademani@users.noreply.github.com>
2023-05-18 23:47:06 +0530

    Add files via upload

---
+++ Course_proj/parameter.v
@@ -0,0 +1,7 @@
+
+`define INPUTFILENAME		 "img.hex" // Input file name
+`define OUTPUTFILENAME		 "output.bmp"		// Output file name
+
+// Choose the operation of code by delete // in the beginning of the selected line
+
+`define BRIGHTNESS_OPERATION
